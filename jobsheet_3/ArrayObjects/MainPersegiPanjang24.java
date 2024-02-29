2e3fd48f15dd43eb8e634363ea241e49aa609384
RizkyaSalsabila <rzkysalsabila@gmail.com>
2024-02-29 10:45:47 +0700

    Modif percobaan 2

--- jobsheet_3/ArrayObjects/MainPersegiPanjang24.java
+++ jobsheet_3/ArrayObjects/MainPersegiPanjang24.java
@@ -3,13 +3,17 @@ public class MainPersegiPanjang24 {
     public static void main(String[] args) {
         //Deklarasi Scanner
         Scanner input24 = new Scanner(System.in); 
-        //Instansiasi ppArray dengan panjang 3
-        PersegiPanjang24[] ppArray = new PersegiPanjang24[3];
+
+        //Input user
+        System.out.print("Masukkan banyaknya persegi panjang : ");
+        int jumPersegiPanjang = input24.nextInt();
+        //Panjang array berdasarkan inputan user
+        PersegiPanjang24[] ppArray = new PersegiPanjang24[jumPersegiPanjang];
 
         //Looping untuk mengisi panjang dan lebar
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < ppArray.length; i++) {
             ppArray[i] = new PersegiPanjang24();
-            System.out.println("Persegi panjang ke - " + i);
+            System.out.println("\nPersegi panjang ke - " + i);
             System.out.print("Masukkan panjang : ");
             ppArray[i].panjang = input24.nextInt();
             System.out.print("Masukkan lebar : ");
@@ -19,7 +23,7 @@ public class MainPersegiPanjang24 {
         System.out.println("------------------------");
         System.out.println("Hasil Tampilan :");
         //Looping untuk mengakses dan menampilkan ppArray
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < ppArray.length; i++) {
             System.out.println("Persegi panjang ke - " + i);
             System.out.println("Panjang : " + ppArray[i].panjang + ", lebar : " + ppArray[i].lebar);
             System.out.println(" ");
