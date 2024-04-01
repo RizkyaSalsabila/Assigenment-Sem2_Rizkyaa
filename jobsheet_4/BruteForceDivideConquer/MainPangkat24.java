d442fa9de1a952fe8c98f50392d53ea18326ace7
RizkyaSalsabila <rzkysalsabila@gmail.com>
2024-04-01 10:19:37 +0700

    Modifikasi percobaan 2 nomer 4

--- jobsheet_4/BruteForceDivideConquer/MainPangkat24.java
+++ jobsheet_4/BruteForceDivideConquer/MainPangkat24.java
@@ -1,7 +1,8 @@
+import java.util.Scanner;
 public class MainPangkat24 {
     public static void main(String[] args) {
-        /*Scanner input24 = new Scanner(System.in);
-        System.out.println("-----------------------------");
+        Scanner input24 = new Scanner(System.in);
+        /*System.out.println("-----------------------------");
         System.out.print("Masukkan jumlah elemen yang dihitung : ");
         int elemen = input24.nextInt();*/
 
@@ -9,7 +10,6 @@ public class MainPangkat24 {
         png[0] = new Pangkat24(2, 4);
         png[1] = new Pangkat24(3, 5);
         png[2] = new Pangkat24(5, 2);
-
 
         /*for (int i = 0; i < elemen; i++) {
             png[i] = new Pangkat24();
@@ -19,26 +19,44 @@ public class MainPangkat24 {
             png[i].pangkat = input24.nextInt();
         }*/
 
-
-        System.out.println("\nHASIL PANGKAT - BRUTE FORCE");
-        for (int i = 0; i < 3; i++) {
-            System.out.println("Hasil dari " 
-            + png[i].nilai + " pangkat " 
-            + png[i].pangkat + " adalah "
-            + png[i].pangkatBF(png[i].nilai, png[i].pangkat)
-            );
-        }
+        System.out.println("-----------------------------");
+        System.out.println("Metode perhitungan yang Anda gunakan : ");
+        System.out.println("1. Metode Brute Force");
+        System.out.println("2. Metode Divide and Conquer");
+        System.out.println("-----------------------------\n");
+        System.out.print("Pilih (1/2) : ");
+        int pilih = input24.nextInt();
+        input24.nextLine();
         System.out.println("-----------------------------");
 
-        System.out.println("\nHASIL PANGKAT - DIVIDE AND CONQUER");
-        for (int i = 0; i < 3; i++) {
-            System.out.println("Hasil dari "
-            + png[i].nilai + " pangkat "
-            + png[i].pangkat + " adalah " 
-            + png[i].pangkatDC(png[i].nilai, png[i].pangkat)
-            );
+        switch (pilih) {
+            case 1:
+                System.out.println("HASIL PANGKAT - BRUTE FORCE");
+                for (int i = 0; i < 3; i++) {
+                    System.out.println("Hasil dari " 
+                    + png[i].nilai + " pangkat " 
+                    + png[i].pangkat + " adalah "
+                    + png[i].pangkatBF(png[i].nilai, png[i].pangkat)
+                    );
+                }
+                System.out.println("-----------------------------");
+                break;
+            case 2:
+                System.out.println("HASIL PANGKAT - DIVIDE AND CONQUER");
+                for (int i = 0; i < 3; i++) {
+                    System.out.println("Hasil dari "
+                    + png[i].nilai + " pangkat "
+                    + png[i].pangkat + " adalah " 
+                    + png[i].pangkatDC(png[i].nilai, png[i].pangkat)
+                    );
+                }
+                System.out.println("-----------------------------");
+                break;
+    
+            default:
+                System.out.println("Input yang Anda masukkan tidak valid. Silahkan isi dengan benar");
+                break;
         }
-        System.out.println("-----------------------------");
-        //input24.close();
+        input24.close();
     }
 }
