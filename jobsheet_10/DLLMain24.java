938c6ef34d135fe24951fc248f355b3f6c099fca
RizkyaSalsabila <rzkysalsabila@gmail.com>
2024-05-15 11:00:44 +0700

    Penambahan method P1 ke P2

--- jobsheet_10/DLLMain24.java
+++ jobsheet_10/DLLMain24.java
@@ -2,6 +2,7 @@ public class DLLMain24 {
     public static void main(String[] args) throws Exception {
         DoubleLinkedList24 dll = new DoubleLinkedList24();
 
+        /*PERCOBAAN 1 
         //panggil method 'print' yang belum terisi data
         dll.print();
         System.out.println("\nSize : " + dll.size());
@@ -38,6 +39,62 @@ public class DLLMain24 {
         dll.clear();
         dll.print();
         System.out.println("\nSize : " + dll.size());
+        System.out.println("------------------------------");
+        */
+
+        //PERCOBAAN 2
+        //panggil method 'print' yang belum terisi data
+        dll.print();
+        System.out.println("\nSize : " + dll.size());
+        System.out.println("------------------------------");
+
+        //panggil method addLast
+        System.out.println("Tambah data '50' di akhir");
+        dll.addLast(50);
+        dll.print();
+        System.out.println("Size : " + dll.size());
+        System.out.println("------------------------------");
+
+        //panggil method addLast
+        System.out.println("Tambah data '40' di akhir");
+        dll.addLast(40);
+        dll.print();
+        System.out.println("Size : " + dll.size());
+        System.out.println("------------------------------");
+
+        //panggil method addLast
+        System.out.println("Tambah data '10' di akhir");
+        dll.addLast(10);
+        dll.print();
+        System.out.println("Size : " + dll.size());
+        System.out.println("------------------------------");
+        
+        //panggil method addLast
+        System.out.println("Tambah data '20' di akhir");
+        dll.addLast(20);
+        dll.print();
+        System.out.println("Size : " + dll.size());
+        System.out.println("------------------------------");
+
+        //panggil method removeFirst
+        System.out.println("Penghapusan data awal '50'");
+        dll.removeFirst();
+        dll.print();
+        System.out.println("Size : " + dll.size());
+        System.out.println("------------------------------");
+
+        //panggil method removeLast
+        System.out.println("Penghapusan data akhir '20'");
+        dll.removeLast();
+        dll.print();
+        System.out.println("Size : " + dll.size());
+        System.out.println("------------------------------");
+
+        //panggil method remove
+        System.out.println("Penghapusan data di indeks ke 1/data 2 '10'");
+        dll.remove(1);
+        dll.print();
+        System.out.println("Size : " + dll.size());
         System.out.println("------------------------------");
     }
 }
--- jobsheet_10/DoubleLinkedList24.java
+++ jobsheet_10/DoubleLinkedList24.java
@@ -97,4 +97,61 @@ public class DoubleLinkedList24 {
             System.out.println("\n\nData berhasil diisi ^-^");
         }
     }
+
+    //method untuk menghapus pada data yang di awal 'removeFirst'
+    public void removeFirst() throws Exception{
+        if (isEmpty()) {
+            throw new Exception("Linked lists masih kosong, data tidak dapat dihapus !!");
+        } else if (size == 1) {
+            removeLast();
+        } else {
+            head = head.next;
+            head.prev = null;
+            size--;
+        }
+    }
+
+    //method untuk menghapus pada data yang di akhir 'removeLast'
+    public void removeLast() throws Exception {
+        if (isEmpty()) {
+            throw new Exception("Linked lists masih kosong, data tidak dapat dihapus !!");
+        } else if (head.next == null) {
+            head = null;
+            size--;
+            return;
+        }
+        Node24 current = head;
+        while (current.next.next != null) {
+            current = current.next;
+        }
+        current.next = null;
+        size--;
+    }
+
+    //method untuk menghapus pada data sesuai indeks tertentu 'remove'
+    public void remove(int index) throws Exception {
+        if (isEmpty() || index >= size) {
+            throw new Exception("Nilai indeks yang diinput di luar batas");
+        } else if (index == 0) {
+            removeFirst();
+        } else {
+            Node24 current = head;
+            int i = 0;
+            while (i < index) {
+                current = current.next;
+                i++;
+            }
+            if (current.next == null) {
+                current.prev.next = null;
+            } else if (current.prev == null) {
+                current = current.next;
+                current.prev = null;
+                head = current;
+            } else {
+                current.prev.next = current.next;
+                current.next.prev = current.prev;
+            }
+            size--;
+        }
+    }
 }
