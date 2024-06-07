9c7a21d27d0a660804111289e3a4d45617a22a5a
RizkyaSalsabila <rzkysalsabila@gmail.com>
2024-06-07 08:15:58 +0700

    Modif p1 nomer 2

--- jobsheet_14/percobaan1/ContohList24.java
+++ jobsheet_14/percobaan1/ContohList24.java
@@ -7,11 +7,12 @@ import java.util.List;
 
 public class ContohList24 {
     public static void main(String[] args) {
-        List l = new ArrayList();
+        List<Integer> l = new ArrayList<> ();
         l.add(1);
         l.add(2);
         l.add(3);
-        l.add("Cireng");
+        //l.add("Cireng");
+        l.add(5);
         System.out.printf("Elemen 0: %d total elemen: %d elemen terakhir: %s\n", l.get(0), l.size(), l.get(l.size() - 1));
 
         l.add(4);
